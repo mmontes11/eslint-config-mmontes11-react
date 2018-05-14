5b69071039cb6f4d44e97f92c6b63fada6e93f3e
mmontes11 <martin11lrx@gmail.com>
2018-05-14 22:15:07 +0200

    Aded import/resolver for webpack

--- index.js
+++ index.js
@@ -7,9 +7,28 @@ module.exports = {
   extends: ["airbnb", "prettier", "prettier/react"],
   plugins: ["import", "jsx-a11y", "prettier", "react"],
   rules: {
+    "import/extensions": [
+      "error",
+      "always",
+      {
+        "js": "never",
+        "jsx": "never"
+      }
+
+    ],
     "import/prefer-default-export": 0,
     "no-underscore-dangle": 0,
     "no-console": 1,
     "prettier/prettier": "error",
+    "react/jsx-filename-extension": 0
   },
+  "import/resolver": {
+    webpack: {
+      config: "webpack.config.js",
+      extensions: [
+        ".js",
+        ".jsx"
+      ]
+    }
+  }
 };
